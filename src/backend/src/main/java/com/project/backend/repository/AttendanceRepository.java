cf157f61e700e1e817f16b09da30bf272b47972e
Qs11s <3116137570@qq.com>
2025-12-04 17:07:46 +0800

    Create AttendanceRepository.java

---
+++ src/backend/src/main/java/com/project/backend/repository/AttendanceRepository.java
@@ -0,0 +1,18 @@
+package com.project.backend.repository;
+
+import com.project.backend.model.Attendance;
+import com.project.backend.model.SessionEntity;
+import com.project.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+    List<Attendance> findByStudent(User student);
+
+    List<Attendance> findBySession(SessionEntity session);
+
+    Optional<Attendance> findByStudentAndSession(User student, SessionEntity session);
+}
