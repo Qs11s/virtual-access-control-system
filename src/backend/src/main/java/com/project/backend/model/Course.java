455a9b3b6ff06f2e3e884d615990d5ca1dfdf1d5
Qs11s <3116137570@qq.com>
2025-12-04 16:58:30 +0800

    Create Course.java

---
+++ src/backend/src/main/java/com/project/backend/model/Course.java
@@ -0,0 +1,18 @@
+package com.project.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "courses")
+@Data
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+}
